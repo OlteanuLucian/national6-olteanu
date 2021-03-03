f356447fb7ab1f700e87757ea0ad1086a26987d6
OlteanuLucian <vlucian.olteanu@gmail.com>
2021-03-03 19:35:53 +0200

    Finished homework_digitalClock_timer

--- digitalClock.js
+++ digitalClock.js
@@ -1,27 +1,18 @@
 console.log("JavaScript - Digital Clock");
 
+//Declared variables list 
 let seconds = 0;
-const secondsParagraphs = document.querySelectorAll(".seconds p");
+const secondsParagraphs = document.querySelectorAll(".seconds span");
 
 let minutes = 0;
-const minutesParagraphs = document.querySelectorAll(".minutes p");
+const minutesParagraphs = document.querySelectorAll(".minutes span");
 
 let hours = 0;
-const hoursParagraphs = document.querySelectorAll(".hours p");
+const hoursParagraphs = document.querySelectorAll(".hours span");
 
 let t;
 
-
-// //define variable to hold setInterval() function
-// let interval = null;//null because we do not want the function to run when the user loads the page 
-// //define variable to hold stopwatch status
-// let status = "stopped";
-
-
-
-
-
-//stopwatch function (logic to determine when to increment next value)
+//Stopwatch function (logic to determine when to increment next value)
 function stopwatchTimer () {
   renderDigits(seconds, secondsParagraphs);
   renderDigits(minutes, minutesParagraphs);
@@ -44,10 +35,10 @@ function stopwatchTimer () {
   }
 };
 
-//function to display updated time values to user
+//Function to display updated time values to user
 function renderDigits(nr, pList) {
-  const stringDigits = nr + "";//transforma numarul in string, si concateneaza nr cu string gol
-  const digitList = stringDigits.split("");//transformam valoarea lui stringDigits intr-un array cu 1 sau 2 elemente; asta face metoda split
+  const stringDigits = nr + "";
+  const digitList = stringDigits.split("");
 
   if (digitList.length === 2) {
     pList[0].innerText = digitList[0];
@@ -58,62 +49,32 @@ function renderDigits(nr, pList) {
   }
 }
 
-
-//start button
-// document.getElementById("button-start").addEventListener("click", startBtn);
-// function startBtn(){
-//     // if (status === "stopped"){
-//         //start the stopwatch by calling the setInterval() function
-//         interval = window.setInterval(stopWatch, 100);
-//         // document.getElementById("button-start").innerHTML = "Start";
-//     //     status = "stopped";
-//     // }
-// };
-
-
-// //stop button
-// document.getElementById("button-stop").addEventListener("click", stopBtn);
-// function stopBtn(){
-//     if (status === "started"){
-//         //start the stopwatch by calling the setInterval() function
-//         window.clearInterval(interval);
-       
-//     }
-// };
-
+//Start timer when click on Start btn
 document.getElementById("button-start").addEventListener("click", startTime);
 function startTime(){
-    t = setInterval(stopwatchTimer,100);
-    // document.getElementById("button-start").disabled = true;
+  t = setInterval(stopwatchTimer,10);
 }
 
+//Stop timer when click on Stop btn
 document.getElementById("button-stop").addEventListener("click", stopTime);
 function stopTime(){
-    clearInterval(t);
-    // document.getElementById("button-start").disabled = true;
+  clearInterval(t);
 }
 
- document.getElementById("button-reset").addEventListener("click", resetTime);
- function resetTime(){
-     clearInterval(t);
-     seconds = 0;
-     minutes = 0;
-     hours = 0;
+//Reset interval  and subsequently reset list with lap values when click on Reset btn
+document.getElementById("button-reset").addEventListener("click", resetTime);
+  function resetTime(){
+    clearInterval(t);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
     stopwatchTimer();
- }
-
-
- document.getElementById("button-save").addEventListener("click", saveTime);
- function saveTime(){ 
-  
+    laps.innerHTML= " "; 
   }
- 
 
-
-
-
-
-
-//  document.getElementById("button-save").addEventListener("click", function(){
-//       console.log(`${hours}:${minutes}:${seconds}`);
-//   }) //prints in console
+//  Split function when click on Save btn
+let splitTime = document.getElementById('button-save');
+let split = document.getElementById('laps');
+splitTime.onclick = function() {
+  split.innerHTML += "<li>" + " " + hrs.innerHTML +  min.innerHTML +  sec.innerHTML + "</li>";
+}
