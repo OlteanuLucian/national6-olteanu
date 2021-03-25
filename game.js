893d4080e65bad8df07c1d092d9e54c3852a51fc
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-03-25 20:01:26 +0200

    break

--- game.js
+++ game.js
@@ -46,13 +46,64 @@ class Player  extends GameObject {
 class Obstacle extends GameObject{
     constructor() {
         super();//cuvant cheie care apeleaza si constructorul clasei de baza GameObject pe langa constructorul clasei Player
-        this.ref.style.background ="red";
+        this.ref.style.background = "red";
         this.move(1055, 55);
     }
     moveLeft(){
         this.move(this.x-5, this.y)
     };
 }
+
+//generatorul de obstacole
+class ObstacleFactory {
+    constructor() {
+        this.obstacles = [];
+    }
+
+    createObstacle() {
+        const obstacle = new Obstacle();
+        obstacle.move(1060, Math.floor(Math.random() * 450));
+        this.obstacles.push(obstacle);        
+    }
+    //making obstacles disappear outside the scene
+    destroyObstacles() {
+        this.obstacles = this.obstacles.filter((obstacle)=> {
+            if (obstacle.x < -50) {
+                obstacle.removeRef();//sterge obiectul JS
+                return false;
+            }
+
+            return true;
+        });
+    }
+
+    moveObstacles(){
+            for(const obstacle of this.obstacles) {
+            obstacle.moveLeft();
+        }
+    }   
+    //removeRef distruge obiectul HTML. acesta se distruge inaintea oboectului JS
+    removeRef() {
+        this.ref.remove();
+    }
+}
+
+///collision detection
+
+function collisionDetection(player, obstacles) {
+    for(const obstacle of obstacles) {
+        
+        if ((player.x >= (obstacle.x - obstacle.width)) &&
+            ((player.x + player.width) >= obstacle.x) && 
+            (player.y <= (obstacle.y + obstacle.height)) && 
+            ((player.y + player.height) >= obstacle.y) )   
+        return true;
+    }
+
+    return false;
+}
+
+
 
 //User  Input
 let keyUpPress = false;
@@ -82,16 +133,34 @@ document.addEventListener("keyup", (event) => {
 
 
 
-//instantiate Player, Obstacle
+//instantiate Player, Obstacle 
 const player = new Player();
-const obstacle = new Obstacle();
+//const obstacle = new Obstacle();
+const obstacleFactory=new ObstacleFactory();
 
-//cream un loop pentru miscarea obstacolelor
-setInterval(()=>{
+
+
+//-GAME LOOP----cream un loop pentru miscarea obstacolelor
+let count = 0;
+
+let gameLoop = setInterval(()=>{
     console.log(keyUpPress);
+
     if(keyUpPress)player.moveUp();
     if(keyDownPress)player.moveDown();
-
+    if (count % 10 === 0)obstacleFactory.createObstacle();
     //moving the obstacles
-    obstacle.moveLeft();
-},250);
+//     obstacle.moveLeft();
+// }, 250);//setInterval apeleaza functia la fiecare 250 milisec
+
+obstacleFactory.moveObstacles();
+
+if(collisionDetection (player, obstacleFactory.obstacles)) {
+    clearInterval(gameLoop);
+    alert("You have been hit");
+    window.location = "/";
+}
+
+obstacleFactory.destroyObstacles();
+count++;
+}, 100);
