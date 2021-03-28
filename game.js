4b60e6ccd755d67acdd3cfbcb6b23bdaa2155f70
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-03-28 17:22:44 +0300

    finished homework

--- game.js
+++ game.js
@@ -1,8 +1,40 @@
-console.log("Javascript OOP - Spaceship Game");
+class Spaceship {
+    constructor() {
+        this.xPosition = 0;
+        this.yPosition = 0;
+        this.generateHtmlRef();
+        this.setMoveSpaceship()
+    }
+    generateHtmlRef() {
+        const spaceship = ['blue_spaceship.png','red_spaceship.png','green_spaceship.png']  
+        this.ref = document.createElement("img");
+        this.ref.src = spaceship[Math.floor(Math.random() * spaceship.length)];
+        this.ref.classList.add("starship");
+        document.body.appendChild(this.ref);
+    }
+    setMoveSpaceship() {
+        document.addEventListener("keydown", (event) => {
+            newSpaceship.moveSpaceship(event.key)
+        });
+    }
+    moveSpaceship(direction) {
+        if (direction === "ArrowRight") {
+            this.xPosition += 5;
+            this.ref.style.transform = `translate(${this.xPosition}px, ${this.yPosition}px)`;
+        } else if (direction === "ArrowLeft") {
+            this.xPosition -= 5;
+            this.ref.style.transform = `translate(${this.xPosition}px, ${this.yPosition}px)`;
+        } else if (direction === "ArrowUp") {
+            this.yPosition -= 5;
+            this.ref.style.transform = `translate(${this.xPosition}px, ${this.yPosition}px)`;
+        } else if (direction === "ArrowDown") {
+            this.yPosition += 5;
+            this.ref.style.transform = `translate(${this.xPosition}px, ${this.yPosition}px)`;
+        }
+    }    
+};
 
-document.getElementById("generate-starship").addEventListener("click", generateStarship);
-let actualStarship;
-
-function generateStarship(){
-    actualStarship = new Spaceship();
-}
+let newSpaceship;
+document.getElementById('generate-starship').addEventListener('click', function() {
+    newSpaceship = new Spaceship();
+});
