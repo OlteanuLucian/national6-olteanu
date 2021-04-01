d523cc97295dd50dee44066e4cdea1240940f1a8
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-04-01 19:34:22 +0300

    2nd commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("JavaScript-Cookies & Local Storage");
+
+console.log(document.cookie);
+
+document.getElementById("logout").addEventListener("click",()=> {
+    document.cookie = "password=123Luc; Expires=Wed, 21 Oct 2015 07:28:00 GMT";
+    window.location ="/"// "/"" = root
+})
