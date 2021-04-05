20560febc065a27c65e97375b4785f147dcc3325
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-04-05 12:30:38 +0300

    last update

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-console.log("JavaScript-Cookies & Local Storage");
+console.log("JavaScript - Cookies & LocalStorage");
 
 console.log(document.cookie);
 
-//document.cookie = "test=123";
+if (!document.cookie) {
+  window.location = "/login.html";
+}
 
-if(!document.cookie) {
+if (document.cookie) {
+  const cookiesElements = document.cookie.split("; ");
+  console.log(cookiesElements);
+  const cookieObj = {};
+  for (const element of cookiesElements) {
+    const [key, value] = element.split("=");
+    cookieObj[key] = value;
+    console.log(key, value);
+  }
+  console.log(cookieObj);
+
+  if (!cookieObj.name || !cookieObj.password) {
     window.location = "/login.html";
+  }
 }
 
-if(document.cookie) {
-    const cookiesElements = document.cookie.split("; ");
-    console.log(cookiesElements);
-    const cookieObject = {};
-    for (const element of cookiesElements) {
-        const [key, value] = element.split("=");
-        cookieObject[key] = value;
-        console.log(key, value);
-    }
-    console.log(cookieObject);
-
-    if(!cookieObject.name || !cookieObject.password){
-        window.location = "/login.html";
-    }
-}
-
-document.getElementById("logout").addEventListener("click",()=> {
-    document.cookie = "name=jhlnjgkh; Expires=Wed, 31 Oct 1990 07:28:00 GMT";
-    window.location ="/"// "/"" = root
-    document.cookie = "password=123; Expires=Wed, 31 Oct 1990 07:28:00 GMT";
-    window.location ="/"// "/"" = root
+document.getElementById("logout").addEventListener("click", () => {
+  document.cookie = "password=123; ; Expires=Thu, 31 Oct 1990 07:28:00 GMT;";
+  document.cookie = "name=123; ; Expires=Thu, 31 Oct 1990 07:28:00 GMT;";
+  window.location = "/";
 });
