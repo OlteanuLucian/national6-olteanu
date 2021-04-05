62dbef237fa2fefa4cb39ad3a8d729318d6d3c84
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-04-05 12:31:35 +0300

    last update

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-console.log("JavaScript-Cookies & Local Storage");
+console.log("JavaScript - LocalStorage");
 
-if(!localStorage.getItem("name")|| ! localStorage.getItem("password")) {
-    window.location = "/login.html";
+
+// Here will attempt the some logic as for the branch: course-21-cookies, but using local-storage
+// Local-storage is similar to cookies but is easier to work with and can store more data
+
+// checkout handleLogin to see how to set items in localStorage
+
+if (!localStorage.getItem("name") || !localStorage.getItem("password")) {
+ window.location = '/login.html';
 }
 
 document.getElementById("logout").addEventListener("click", () => {
   localStorage.removeItem("name");
-  localStorage.removeItem("password");
-
-  window.location ="/";
+  localStorage.removeItem('password');
+  window.location = "/";
 });
