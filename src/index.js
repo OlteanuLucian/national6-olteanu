0d5ff789347c5a7b2bc4bb3999865207a0065947
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-04-08 20:42:41 +0300

    clean start

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-console.log("Entry point to webpack");
-import { addPixel, addPercentage } from "./utils/addUnitsType";
-
-const aRandomNumber = Math.floor(Math.random() * 20);
-console.log(aRandomNumber);
-
-const aRandomNumberWithPixel = addPixel(aRandomNumber);
-console.log(aRandomNumberWithPixel);
-
-const aRandomNumberWithPercentage = addPercentage(aRandomNumber);
-console.log(aRandomNumberWithPercentage);
-
-//always run npm run build to check in the console
+console.log("To do app");
