8965e880c5b1c609e615860d6d0e8d4cad8a1905
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-04-14 13:48:54 +0300

    working tree

---
+++ src/utils/api.js
@@ -0,0 +1,13 @@
+function getDataFromServer(url,callback) {
+    fetch(url)
+        .then((r)=>r.json())
+        .then(callback)
+}
+
+export function getBreedListFromServer(callback) {
+    getDataFromServer("https://dog.ceo/api/breeds/list/all",callback)
+}
+
+export function getBreedImagesFromServer(callback){
+    getDataFromServer(`https://dog.ceo/api/breed/${localStorage.breed}/images`,callback)
+}
---
+++ src/utils/breeds.js
@@ -0,0 +1,69 @@
+import { getBreedImagesFromServer } from "./utils/api";
+
+let currentBreedImage = [];
+
+export function renderBreeds(breedList) {
+    for (const breed of Object.keys(breedList.message)) {
+        renderBreed(breed);
+    }
+    if (localStorage.breed && localStorage.index) {
+        document.getElementById(localStorage.breed).classList.add("breed--selected");
+        document.getElementById("page-number").innerText = "";
+        document.getElementById("page-number").innerText = parseInt(localStorage.index) + 1;
+        getBreedImagesFromServer(renderImage);
+    }
+}
+
+export function renderBreed(dogBreed) {
+    const breedName = document.createElement("p");
+    breedName.innerText = dogBreed;
+    breedName.setAttribute("id", dogBreed);
+    document.getElementById("breeds").appendChild(breedName);
+    breedName.addEventListener("click", selectBreed);
+}
+
+export function selectBreed() {
+    if (document.querySelector(".breed--selected")) {
+        document.querySelector(".breed--selected").classList.remove("breed--selected");
+    }
+    localStorage.setItem("index", 0);
+    modifyPageNumber();
+    localStorage.setItem("breed", this.id);
+    this.classList.add("breed--selected");
+    getBreedImagesFromServer(renderImage);
+}
+
+
+export function renderImage(imageResponseMessage) {
+    currentBreedImage = imageResponseMessage.message;
+    if (localStorage.index) {
+        document.getElementById("breed-image").setAttribute("src", currentBreedImage[localStorage.index]);
+    }
+    else {
+        document.getElementById("breed-image").setAttribute("src", currentBreedImage[0]);
+    }
+}
+
+export function backwardImage() {
+    if (document.querySelector(".breed--selected")) {
+        if (localStorage.index >= 1) {
+            localStorage.index--;
+        }
+        modifyPageNumber();
+    }
+}
+
+export function forwardImage() {
+    if (document.querySelector(".breed--selected")) {
+        if (localStorage.index < currentBreedImage.length - 1) {
+            localStorage.index++;
+        }
+        modifyPageNumber();
+    }
+}
+
+function modifyPageNumber() {
+    document.getElementById("page-number").innerText = "";
+    document.getElementById("page-number").innerText = parseInt(localStorage.index) + 1;
+    document.getElementById("breed-image").setAttribute("src", currentBreedImage[localStorage.index]);
+}
