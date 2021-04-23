0b61e26aa534eddabfd39e3ddbbd733462247f3b
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-04-23 08:17:53 +0300

    fucntionality to menuBtn

--- src/components/Header/Menu/MenuButton/MenuButton.jsx
+++ src/components/Header/Menu/MenuButton/MenuButton.jsx
@@ -5,6 +5,17 @@ import menuIcon from "./menu.svg"
 
 export function MenuButton() {
     return(
-        
+        <div
+            onMouseEnter = {() => {
+            document.querySelector(".app-menu-flyout").classList.add("app-menu-flyout--hover");
+            }}
+
+            onMouseLeave = {() => {
+                document.querySelector(".app-menu-flyout--hover").classList.remove("app-menu-flyout--hover")
+            }}
+
+            className = "app-menu-button">
+                <img src = {menuIcon} alt = "menu-icon" className="menu-button" />
+        </div>
     )
 }
