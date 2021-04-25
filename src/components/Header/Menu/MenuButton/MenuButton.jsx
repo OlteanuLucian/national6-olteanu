d0db51ae4d56ffabc47fefba0cc63a272abe25f2
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-04-25 18:40:57 +0300

    finish homework

--- src/components/Header/Menu/MenuButton/MenuButton.jsx
+++ src/components/Header/Menu/MenuButton/MenuButton.jsx
@@ -1,20 +1,12 @@
-import React from 'react';
-import "./MenuButton.css"
-import menuSvg from "./menu.svg"
 
-export function MenuButton() {
-    return(
-        <div
-        onMouseEnter={() => {
-            document.querySelector(".app-menu-flyout").classList.add("app-menu-flyout--hover");
-          }}
+import menuSvg from "./menu.svg";
+import "./MenuButton.css";
 
-          onMouseLeave={() => {
-            document.querySelector(".app-menu-flyout--hoover").classList.remove("app-menu-flyout--hover");
-          }}
-          
-          className="app-menu-button">
-            <img src={menuSvg} alt="menu" className="menu-button"></img>
+export function MenuButton(props) {
+    return (
+        <div className="app-menu-button" onMouseEnter={props.onMouseEnter} 
+            onMouseLeave={props.onMouseLeave}>
+            <img src={menuSvg} id="menu-button" alt="menu-img" />
         </div>
-    ) 
+    );
 }
--- src/components/Header/Menu/MenuFlyout/MenuFlyout.jsx
+++ src/components/Header/Menu/MenuFlyout/MenuFlyout.jsx
@@ -1,20 +1,11 @@
-import React from "react"
-import "./MenuFlyout.css"
+import "./MenuFlyout.css";
 
 export function MenuFlyout() {
-    return(
-        <div
-            onMouseEnter = {() => {
-            document.querySelector(".app-menu-flyout").classList.add("app-menu-flyout--hover");
-            }}
-
-            onMouseLeave = {() => {
-                document.querySelector(".app-menu-flyout--hover").classList.remove("app-menu-flyout--hover")
-            }}
-
-            className = "app-menu-button">
-                <p>about</p>
-                <p>tasks</p>
+    return (
+        <div className="app-menu-flyout" id="flyout" 
+            style={{ display: "none" }}>
+            <p>about</p>
+            <p>tasks</p>
         </div>
-    )
+    );
 }
